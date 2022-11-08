9603819b3331d0ae802147cfc5ab642216ae92c2
Lucas Leonette da Cruz <88354824+Leo3002@users.noreply.github.com>
2022-11-08 12:42:09 -0300

    Primeiro Commit

---
+++ Biblioteca/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Free Book</title>
+    <link rel="stylesheet" href="css/Index.css">
+</head>
+<body>
+    <?php include 'navbar.php' ?>
+    <section class="bg-livros">
+        <form action="" >
+            <div >
+                <input  class="inputPesquisa" type="text" placeholder="Pesquisar">
+                <button class="buttonPesquisa" type="submit">BUSCAR</button>
+            </div>
+            
+        </form>
+    </section>
+</body>
+</html>
---
+++ Biblioteca/navbar.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="css/nav.css">
+<header>
+      <nav>
+        <a class="logo" href="index.php">Free Book</a>
+        <div class="mobile-menu">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+        <ul class="nav-list">
+          <li><a href="#">Categorias</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+    <script src="js/navbar.js"></script>
